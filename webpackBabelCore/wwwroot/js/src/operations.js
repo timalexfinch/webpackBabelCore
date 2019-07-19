9da2541260858abc7fa7a8ef1604753c7d9f812b
tim finch <tim.finch@rocketmail.com>
2019-07-19 18:42:30 +0100

    updated lodash

--- webpackBabelCore/wwwroot/js/src/operations.js
+++ webpackBabelCore/wwwroot/js/src/operations.js
@@ -1,18 +1,11 @@
-﻿const add = (x, y) => {
-    return x + y;
-};
+﻿const add = (x, y) => x + y;
 
-const subtract = (x, y) => {
-    return x - y;
-};
+const subtract = (x, y) => x - y;
 
-const multiply = (x, y) => {
-    return x * y;
-};
+const multiply = (x, y) => x * y;
 
-const divide = (x, y) => {
-    return x / y;
-};
+const divide = (x, y) => x / y;
+
 
 export default {
     add: add,
@@ -20,3 +13,23 @@ export default {
     multiply: multiply,
     divide: divide
 };
+
+
+// Alternative syntax below:
+
+//const add = (x, y) => {
+//    return x + y;
+//};
+
+//const subtract = (x, y) => {
+//    return x - y;
+//};
+
+//const multiply = (x, y) => {
+//    return x * y;
+//};
+
+//const divide = (x, y) => {
+//    return x / y;
+//};
+
